a87b86af36afee5763d5139b26ccd694e6315dfe
wuyy <wyy2218@126.com>
2024-05-15 04:17:41 -0400

    Task4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,20 +17,38 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn=req.params.isbn
+    res.send(books[isbn])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author=req.params.author;
+    if(author){
+    let keysArray = Object.keys(books);
+    let filteredBook=keysArray.filter(isbn=>books[isbn]["author"]===author
+    )
+    .map(isbn=>books[isbn])
+    res.send(filteredBook);
+    }
+    else{
+        res.send("Unable to find book!")
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title=req.params.title;
+    if(title){
+    let keysArray = Object.keys(books);
+    let filteredBook=keysArray.filter(isbn=>books[isbn]["title"]===title
+    )
+    .map(isbn=>books[isbn])
+    res.send(filteredBook);
+    }
+    else{
+        res.send("Unable to find book!")
+    }
 });
 
 //  Get book review
