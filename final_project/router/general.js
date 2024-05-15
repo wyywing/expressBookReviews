7a26e7acbafd5a51e3877745d8756139f2c0dd56
wuyy <wyy2218@126.com>
2024-05-15 05:18:43 -0400

    Task14

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -41,8 +41,12 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
+    const myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
     const isbn=req.params.isbn
-    res.send(books[isbn])
+    resolve(books[isbn])}, 1000);
+});
+myPromise.then((book)=>{res.send(book)});
  });
   
 // Get book details based on author
