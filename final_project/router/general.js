ec383dbdd5aa30f034384542fa966ae2944ccc9f
wuyy <wyy2218@126.com>
2024-05-15 04:22:55 -0400

    Task5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,17 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
+const doesExist = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
 public_users.post("/register", (req,res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
@@ -53,8 +63,14 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn=req.params.isbn
+    let book=books[isbn]
+    if(book){
+    res.send(book["reviews"])
+    }
+    else{
+        res.send("Unable to find book!")
+    }
 });
 
 module.exports.general = public_users;
